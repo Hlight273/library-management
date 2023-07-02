885567d95f302d21ba314783f2b1158191e76838
usx-lb <3394224539@qq.com>
2023-07-02 14:38:03 +0800

    7.2 查看奖状更新

--- src/main/java/com/servlet/CreteServlet.java
+++ src/main/java/com/servlet/CreteServlet.java
@@ -1,5 +1,6 @@
 package com.servlet;
 
+import com.dao.MatchDao;
 import com.dao.MemberDao;
 import com.dao.TeamDao;
 import com.domain.Match;
@@ -20,8 +21,11 @@ public class CreteServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         User user = (User) request.getSession().getAttribute("user");
-        Match match = (Match) request.getSession().getAttribute("match");
+//        Match match = (Match) request.getSession().getAttribute("match");
+        int matchId = Integer.parseInt(request.getParameter("matchId"));
         int teamId = Integer.parseInt(request.getParameter("teamId"));
+        MatchDao matchDao = new MatchDao();
+        Match match = matchDao.getMatchById(matchId);
         TeamDao teamDao = new TeamDao();
         Team team = teamDao.getTeamById(teamId);
         String pattern = "yyyy年MM月dd日";
